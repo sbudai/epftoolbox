d2f57f510552771b48acc415dd72051bd38b0482
sbudai <sbudai.ga@gmail.com>
2024-02-15 09:24:07 +0100

    comments amended and the syntax was linted a little bit

--- examples/metrics/using_mae.py
+++ examples/metrics/using_mae.py
@@ -2,71 +2,97 @@ from epftoolbox.evaluation import MAE
 from epftoolbox.data import read_data
 import pandas as pd
 
-# Download available forecast of the NP market available in the library repository
-# These forecasts accompany the original paper
-forecast = pd.read_csv('https://raw.githubusercontent.com/jeslago/epftoolbox/master/' + 
-                      'forecasts/Forecasts_NP_DNN_LEAR_ensembles.csv', index_col=0)
+# Download available day-ahead electricity price forecasts of
+# the Nord Pool market available in the library repository.
+# These forecasts accompany the original paper.
+print('market: Nord Pool')
+forecast = pd.read_csv('https://raw.githubusercontent.com/jeslago/epftoolbox/master/'
+                       'forecasts/Forecasts_NP_DNN_LEAR_ensembles.csv', index_col=0)
 
-# Transforming indices to datetime format
-forecast.index = pd.to_datetime(forecast.index)
+# Transforming the dataframe's timestamp indices to datetime format
+forecast.index = pd.to_datetime(arg=forecast.index)
 
-# Reading data from the NP market
-_, df_test = read_data(path='.', dataset='NP', begin_test_date=forecast.index[0], 
+# Reading the real day-ahead electricity price data of the Nord Pool market
+# The scope period should be same as in forecasted data.
+_, df_test = read_data(path='.', dataset='NP', begin_test_date=forecast.index[0],
                        end_test_date=forecast.index[-1])
 
-# Extracting forecast of DNN ensemble and display
+# Extracting the day-ahead electricity price forecasts based on 'DNN Ensemble' model and display
 fc_DNN_ensemble = forecast.loc[:, ['DNN Ensemble']]
+print('fc_DNN_ensemble:', fc_DNN_ensemble, sep='\n')
 
-# Extracting real price and display
+# Extracting the real day-ahead electricity price data and display
 real_price = df_test.loc[:, ['Price']]
+print('real_price:', real_price, sep='\n')
 
-# Building the same datasets with shape (ndays, n_prices/day) 
-# instead of shape (nprices, 1) and display
-fc_DNN_ensemble_2D = pd.DataFrame(fc_DNN_ensemble.values.reshape(-1, 24), 
-                                  index=fc_DNN_ensemble.index[::24], 
-                                  columns=['h' + str(hour) for hour in range(24)])
-real_price_2D = pd.DataFrame(real_price.values.reshape(-1, 24), 
-                             index=real_price.index[::24], 
-                             columns=['h' + str(hour) for hour in range(24)])
-fc_DNN_ensemble_2D.head()
+# Building a 2-dimensional price forecast dataframe with shape (rows: n_days, columns: n_prices/n_day)
+# instead of 1-dimensional shape (rows: n_prices, columns: 1) and display
+fc_DNN_ensemble['column_hour'] = ['h' + h for h in fc_DNN_ensemble.index.strftime('%H').astype(int).astype(str)]
+fc_DNN_ensemble_2D = pd.pivot_table(data=fc_DNN_ensemble, values='DNN Ensemble',
+                                    index=fc_DNN_ensemble.index.strftime('%Y-%m-%d'),
+                                    columns='column_hour', aggfunc='mean', sort=False)
+fc_DNN_ensemble.drop(['column_hour'], axis='columns', inplace=True)
+print('fc_DNN_ensemble_2D:', fc_DNN_ensemble_2D, sep='\n')
+
+# Building a 2-dimensional real price dataframe with shape (rows: n_days, columns: n_prices/n_day)
+# instead of 1-dimensional shape (rows: n_prices, columns: 1) and display
+real_price['column_hour'] = ['h' + h for h in real_price.index.strftime('%H').astype(int).astype(str)]
+real_price_2D = pd.pivot_table(data=real_price, values='Price',
+                               index=real_price.index.strftime('%Y-%m-%d'),
+                               columns='column_hour', aggfunc='mean', sort=False)
+real_price.drop(['column_hour'], axis='columns', inplace=True)
+print('real_price_2D:', real_price_2D, sep='\n')
 
 
-# According to the paper, the MAE of the DNN ensemble for the NP market is 1.667
+# According to the paper, the MAE of the 'DNN Ensemble' day-ahead price forecast for the NP market is 1.667
 # Let's test the metric for different conditions
 
-# Evaluating MAE when real price and forecasts are both dataframes
-MAE(p_pred=fc_DNN_ensemble, p_real=real_price)
+# Evaluating MAE when real day-ahead price and forecasts are both 1-dimensional dataframes
+print('MAE(p_pred=fc_DNN_ensemble, p_real=real_price): {0}'.format(MAE(p_pred=fc_DNN_ensemble, p_real=real_price)))
 
-# Evaluating MAE when real price and forecasts are both numpy arrays
-MAE(p_pred=fc_DNN_ensemble.values, p_real=real_price.values)
+# Evaluating MAE when real day-ahead price and forecasts are both pandas Series
+print('MAE(p_pred=fc_DNN_ensemble.loc[:, "DNN Ensemble"], p_real=real_price.loc[:, "Price"]): {0}'.
+      format(MAE(p_pred=fc_DNN_ensemble.loc[:, 'DNN Ensemble'], p_real=real_price.loc[:, 'Price'])))
 
-# Evaluating MAE when input values are of shape (ndays, n_prices/day) 
-# instead of shape (nprices, 1)
-# Dataframes
-MAE(p_pred=fc_DNN_ensemble_2D, p_real=real_price_2D)
-# Numpy arrays
-MAE(p_pred=fc_DNN_ensemble_2D.values, p_real=real_price_2D.values)
+# Evaluating MAE when real day-ahead price and forecasts are both 1-dimensional numpy arrays
+print('MAE(p_pred=fc_DNN_ensemble.loc[:, "DNN Ensemble"].values, p_real=real_price.loc[:, "Price"].values): {0}'.
+      format(MAE(p_pred=fc_DNN_ensemble.loc[:, 'DNN Ensemble'].values, p_real=real_price.loc[:, 'Price'].values)))
 
-# Evaluating MAE when input values are of shape (nprices,) 
-# instead of shape (nprices, 1)
-# Pandas Series
-MAE(p_pred=fc_DNN_ensemble.loc[:, 'DNN Ensemble'], 
-    p_real=real_price.loc[:, 'Price'])
-# Numpy arrays
-MAE(p_pred=fc_DNN_ensemble.values.squeeze(), 
-    p_real=real_price.values.squeeze())
+# Evaluating MAE when real day-ahead price and forecasts are both 2-dimensional (rows: n_days, columns: n_prices/n_day)
+# DataFrames
+print('MAE(p_pred=fc_DNN_ensemble_2D, p_real=real_price_2D): {0}'.
+      format(MAE(p_pred=fc_DNN_ensemble_2D, p_real=real_price_2D)))
+
+# Evaluating MAE when real day-ahead price and forecasts are both 2-dimensional (rows: n_days, columns: n_prices/n_day)
+# numpy arrays
+print('MAE(p_pred=fc_DNN_ensemble_2D.values.squeeze(), p_real=real_price_2D.values.squeeze()): {0}'.
+      format(MAE(p_pred=fc_DNN_ensemble_2D.values.squeeze(), p_real=real_price_2D.values.squeeze())))
 
 
 # We can also test situations where the MAE will display errors
 
-# Evaluating MAE when real price and forecasts are of different type (numpy.ndarray and pandas.DataFrame)
-MAE(p_pred=fc_DNN_ensemble.values, p_real=real_price)
+# Evaluating MAE when real day-ahead price and forecasts are of different object type
+# (numpy.ndarray and pandas.DataFrame)
+try:
+    print('MAE: {0}'.format(MAE(p_pred=fc_DNN_ensemble.values, p_real=real_price)))
+except TypeError as e:
+    print("TypeError:", e)
 
-# Evaluating MAE when real price and forecasts are of different type (pandas.Series and pandas.DataFrame)
-MAE(p_pred=fc_DNN_ensemble, p_real=real_price.loc[:, 'Price'])
+# Evaluating MAE when real day-ahead price and forecasts are of different object type
+# (pandas.Series and pandas.DataFrame)
+try:
+    print('MAE: {0}'.format(MAE(p_pred=fc_DNN_ensemble, p_real=real_price.loc[:, 'Price'])))
+except TypeError as e:
+    print("TypeError:", e)
 
-# Evaluating MAE when real price and forecasts are both numpy arrays of different size
-MAE(p_pred=fc_DNN_ensemble.values, p_real=real_price.values[1:])
+# Evaluating MAE when real day-ahead price and forecasts are both numpy arrays but of different size
+try:
+    print('MAE: {0}'.format(MAE(p_pred=fc_DNN_ensemble.values, p_real=real_price.values[1:])))
+except ValueError as e:
+    print("ValueError:", e)
 
-# Evaluating MAE when real price and forecasts are both dataframes are of different size
-MAE(p_pred=fc_DNN_ensemble.iloc[1:, :], p_real=real_price)
+# Evaluating MAE when real day-ahead price and forecasts are both DataFrames but of different size
+try:
+    print('MAE: {0}'.format(MAE(p_pred=fc_DNN_ensemble.iloc[1:, :], p_real=real_price)))
+except ValueError as e:
+    print("ValueError:", e)
