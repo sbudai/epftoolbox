441050977aa72006e5cda13f61b059008b9e248f
sbudai <sbudai.ga@gmail.com>
2024-02-18 16:57:50 +0100

    read_data() changed to read_and_split_data()

--- examples/statistical test/plotting_dm.py
+++ examples/statistical test/plotting_dm.py
@@ -1,23 +1,23 @@
 from epftoolbox.evaluation import DM, plot_multivariate_DM_test
-from epftoolbox.data import read_data
+from epftoolbox.data import read_and_split_data
 import pandas as pd
 
 # Generating forecasts of multiple models
 
 # Download available forecast of the NP market available in the library repository
 # These forecasts accompany the original paper
-forecasts = pd.read_csv('https://raw.githubusercontent.com/jeslago/epftoolbox/master/' + 
-                      'forecasts/Forecasts_NP_DNN_LEAR_ensembles.csv', index_col=0)
+forecasts = pd.read_csv('https://raw.githubusercontent.com/jeslago/epftoolbox/master/'
+                        'forecasts/Forecasts_NP_DNN_LEAR_ensembles.csv', index_col=0)
 
 # Deleting the real price field as it the actual real price and not a forecast
 del forecasts['Real price']
 
 # Transforming indices to datetime format
-forecasts.index = pd.to_datetime(forecasts.index)
+forecasts.index = pd.to_datetime(arg=forecasts.index)
 
 # Extracting the real prices from the market
-_, df_test = read_data(path='.', dataset='NP', begin_test_date=forecasts.index[0], 
-                       end_test_date=forecasts.index[-1])
+_, df_test = read_and_split_data(path='.', dataset='NP', begin_test_date=forecasts.index[0],
+                                 end_test_date=forecasts.index[-1])
 
 real_price = df_test.loc[:, ['Price']]
 
