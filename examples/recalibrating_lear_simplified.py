6df66686aefaf1ef39fd016e18d6b77ca3285d7c
sbudai <sbudai.ga@gmail.com>
2024-02-22 21:21:39 +0100

    a little bit linted; comments improved

--- examples/recalibrating_lear_simplified.py
+++ examples/recalibrating_lear_simplified.py
@@ -9,27 +9,29 @@ Simplified example for using the LEAR model for forecasting prices with daily re
 from epftoolbox.models import evaluate_lear_in_test_dataset
 import os
 
-# Market under study. If it not one of the standard ones, the file name
-# has to be provided, where the file has to be a csv file
-dataset = 'PJM'
+# Market under study.
+# If is not one of the standard ones, the file name has to be provided (without '.csv' extension),
+# where the file has to be a csv file
+dataset = 'DE'
+
+# Name of the column in the input dataset that denotes the response variable.
+response = 'Price'
 
 # Number of years (a year is 364 days) in the test dataset.
 years_test = 2
 
 # Number of days used in the training dataset for recalibration
-calibration_window = 364 * 4
+calibration_window = 364 * 3
 
-# Optional parameters for selecting the test dataset, if either of them is not provided, 
-# the test dataset is built using the years_test parameter. They should either be one of
-# the date formats existing in python or a string with the following format
-# "%d/%m/%Y %H:%M"
-begin_test_date = None
-end_test_date = None
-
+# Define the path of the input dataset folder
 path_datasets_folder = os.path.join('.', 'datasets')
+
+# Define the path to the folder where to save the forecasts
 path_recalibration_folder = os.path.join('.', 'experimental_files')
     
-evaluate_lear_in_test_dataset(path_recalibration_folder=path_recalibration_folder, 
-                             path_datasets_folder=path_datasets_folder, dataset=dataset, years_test=years_test, 
-                             calibration_window=calibration_window, begin_test_date=begin_test_date, 
-                             end_test_date=end_test_date)
+evaluate_lear_in_test_dataset(path_datasets_folder=path_datasets_folder,
+                              path_recalibration_folder=path_recalibration_folder,
+                              dataset=dataset,
+                              response=response,
+                              calibration_window=calibration_window,
+                              years_test=years_test)
