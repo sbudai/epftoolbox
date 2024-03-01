f99e469f44dfb89719d8ec46e4b46de466398bf5
sbudai <sbudai.ga@gmail.com>
2024-03-01 19:41:13 +0100

    begin_test_date and end_test_date format changed from day_first=True to day_first=False

--- examples/recalibrating_dnn_simplified.py
+++ examples/recalibrating_dnn_simplified.py
@@ -12,7 +12,8 @@ import os
 # Number of layers in DNN
 nlayers = 2
 
-# Market under study. If it not one of the standard ones, the file name
+# Market under study.
+# If it is not one of the standard ones, the file name
 # has to be provided, where the file has to be a csv file
 dataset = 'PJM'
 
@@ -34,25 +35,25 @@ new_recalibration = 1
 # Number of years used in the training dataset for recalibration
 calibration_window = 4
 
-# Unique identifier to read the trials file of hyperparameter optimization
+# Unique identifier to read the trial file of hyperparameter optimization
 experiment_id = 1
 
 # Optional parameters for selecting the test dataset, if either of them is not provided, 
 # the test dataset is built using the years_test parameter. They should either be one of
 # the date formats existing in python or a string with the following format
-# "%d/%m/%Y %H:%M"
-begin_test_date = '27/12/2016'
-end_test_date = '01/03/2017'
+# "%Y-%m-%d %H:%M"
+begin_test_date = '2016-12-27'
+end_test_date = '2017-03-01'
 
-# Set up the paths for saving data (this are the defaults for the library)
+# Set up the paths for saving data (these are the defaults for the library)
 path_datasets_folder = os.path.join('.', 'datasets')
 path_recalibration_folder = os.path.join('.', 'experimental_files')
 path_hyperparameter_folder = os.path.join('.', 'experimental_files')
 
-evaluate_dnn_in_test_dataset(experiment_id, path_hyperparameter_folder=path_hyperparameter_folder, 
-                               path_datasets_folder=path_datasets_folder, shuffle_train=shuffle_train, 
-                               path_recalibration_folder=path_recalibration_folder, 
-                               nlayers=nlayers, dataset=dataset, years_test=years_test, 
-                               data_augmentation=data_augmentation, calibration_window=calibration_window, 
-                               new_recalibration=new_recalibration, begin_test_date=begin_test_date, 
-                               end_test_date=end_test_date)
+evaluate_dnn_in_test_dataset(experiment_id=str(experiment_id), path_hyperparameter_folder=path_hyperparameter_folder,
+                             path_datasets_folder=path_datasets_folder, shuffle_train=shuffle_train,
+                             path_recalibration_folder=path_recalibration_folder,
+                             nlayers=nlayers, dataset=dataset, years_test=years_test,
+                             data_augmentation=data_augmentation, calibration_window=calibration_window,
+                             new_recalibration=new_recalibration, begin_test_date=begin_test_date,
+                             end_test_date=end_test_date)
